d04ce0346fc2b6b3f68b4c6c47933f3bafd6bbae
Danny Denenberg <dannydenenberg@gmail.com>
2019-10-02 07:42:13 -0500

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -61,7 +61,7 @@ class Layout extends React.Component {
             }}
             to={'/'}
           >
-            {title}
+            Danny Denenberg
           </Link>
         </h3>
       );
